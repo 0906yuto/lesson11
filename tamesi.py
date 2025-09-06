56e7c951cb9203df72138bdb148a3609d57ea981
yuto <yuuyama20132020@gmail.com>
2025-09-06 16:49:51 +0900

    tamesi.pyに予定を追加

--- tamesi.py
+++ tamesi.py
@@ -1,10 +1,20 @@
 import streamlit as st
 import streamlit_calendar as st_calendar
 
-st_calendar.calendar()
+
 event1={
-    "id":"1",
-    "title":"robodan",
-    "start":"2025-09-06T15:00:00",
-    "end":"2025-09-06T18:00:00",
+    'id': '1', # イベントを識別するためのID。重複不可
+    'title': 'Data Cloud World Tour', # イベント名
+    'start': '2025-09-08T09:00:00', # 時間帯も指定する
+    'end': '2025-09-08T20:00:00', # 時間帯も指定する
 }
+# calendarにはイベント一覧を配列にして渡す
+event_list = [event1]
+
+# options = {
+#     'initialView': 'timeGridWeek',
+# }
+
+# イベントを表示するカレンダーを作成
+cal = st_calendar.calendar(events=event_list)
+st.write(cal)
