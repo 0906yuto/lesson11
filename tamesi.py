51fdf7a35318b6a5c9865818bbf9b3429bcbd267
yuto <yuuyama20132020@gmail.com>
2025-09-06 16:31:16 +0900

    tamesi.pyにカレンダーの追加

--- tamesi.py
+++ tamesi.py
@@ -1,19 +1,4 @@
 import streamlit as st
-from datetime import date, timedelta
+import streamlit_calendar as st_calendar
 
-# 基本的な使い方
-selected_date = st.date_input("好きな日付を選んでね")
-st.write(f"選んだ日付: {selected_date}")
-
-# デフォルト値と範囲の設定
-today = date.today()
-next_week = today + timedelta(days=7)
-next_month = today + timedelta(days=30)
-
-event_date = st.date_input(
-    "イベントの日付はいつ？",
-    value=next_week,  # 最初に表示される日付
-    min_value=today,  # これより前の日付は選べないよ
-    max_value=next_month  # これより後の日付は選べないよ
-)
-st.write(f"イベント日: {event_date}")
+st_calendar.calendar()
