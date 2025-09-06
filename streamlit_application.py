0b421055d87710dfdfa3714849a8d543753e92df
yuto <yuuyama20132020@gmail.com>
2025-09-06 18:01:17 +0900

    内容の変更

--- streamlit_application.py
+++ streamlit_application.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import streamlit_calendar as st_calendar
 
-
+date=st.date_input("始めの日付")
+time=st.time_input("始めの時間")
+hiduke=st.date_input("終わりの日付")
+zikan=st.time_input("終わりの時間")
 event1={
     'id': '1', # イベントを識別するためのID。重複不可
     'title': 'Data Cloud World Tour', # イベント名
-    'start': '2025-09-08T09:00:00', # 時間帯も指定する
-    'end': '2025-09-11T20:00:00', # 時間帯も指定する
+    'start': f'{date}T{time}', # 時間帯も指定する
+    'end': f'{hiduke}T{zikan}', # 時間帯も指定する
 }
 # calendarにはイベント一覧を配列にして渡す
 event_list = [event1]
