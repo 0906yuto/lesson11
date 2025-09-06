b3cd5bbc51c055c785a826f0ab8681919b20d9ef
yuto <yuuyama20132020@gmail.com>
2025-09-06 17:02:06 +0900

    streamlitapplicationの削除

--- streamlit_application.py
+++ streamlit_application.py
@@ -1,1 +1,20 @@
 import streamlit as st
+import streamlit_calendar as st_calendar
+
+
+event1={
+    'id': '1', # イベントを識別するためのID。重複不可
+    'title': 'Data Cloud World Tour', # イベント名
+    'start': '2025-09-08T09:00:00', # 時間帯も指定する
+    'end': '2025-09-08T20:00:00', # 時間帯も指定する
+}
+# calendarにはイベント一覧を配列にして渡す
+event_list = [event1]
+
+# options = {
+#     'initialView': 'timeGridWeek',
+# }
+
+# イベントを表示するカレンダーを作成
+cal = st_calendar.calendar(events=event_list)
+st.write(cal)
