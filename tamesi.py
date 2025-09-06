b766337ddba08016953776cc682ead0e253f89b1
yuto <yuuyama20132020@gmail.com>
2025-09-06 16:38:04 +0900

    tamesi.pyの予定を追加

--- tamesi.py
+++ tamesi.py
@@ -2,3 +2,9 @@ import streamlit as st
 import streamlit_calendar as st_calendar
 
 st_calendar.calendar()
+event1={
+    "id":"1",
+    "title":"robodan",
+    "start":"2025-09-06T15:00:00",
+    "end":"2025-09-06T18:00:00",
+}
